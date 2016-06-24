9ef3de17f37e13f2e510dac269136bbf508859d7
Will Patterson <wpatt2@pdx.edu>
2016-06-23 21:59:55 -0700

    Removed longdescription

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
+
 setup(name="ImageFudge",
       version="0.0.1",
       description="Image glitching library",
@@ -9,5 +10,4 @@ setup(name="ImageFudge",
       packages=find_packages(),
       package_data={},
       install_requires=["pillow"],
-      long_description=long_description(),
       entry_points={"console_scripts": [""],})
