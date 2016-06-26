372668378bd8029f60b82218617569573b4b92b2
Will Patterson <wpatt2@pdx.edu>
2016-06-25 17:19:27 -0700

    console script to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 
-setup(name="ImageFudge",
-      version="0.0.1",
-      description="Image glitching library",
-      license="MIT",
-      author="William Patterson, Konstantin Farrell, Sean Sission",
+setup(name='ImageFudge',
+      version='0.0.1',
+      description='Image glitching library',
+      license='MIT',
+      author='William Patterson, Konstantin Farrell, Sean Sission',
       packages=find_packages(),
       package_data={},
-      install_requires=["pillow"],
-      entry_points={"console_scripts": [""],})
+      install_requires=['pillow'],
+      entry_points={'console_scripts': ['imagefudge=imagefudge.__main__:main'],})
