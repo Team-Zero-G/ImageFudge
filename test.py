14a2d0498bf963470d61ad7b0cde19dfdac55201
konstantinfarrell <konstantinvonmolotov@gmail.com>
2016-06-30 23:21:41 -0700

    Add docstrings to tests

--- test.py
+++ test.py
@@ -18,19 +18,31 @@ class TestFudged(unittest.TestCase):
         self.fudge.image.close()
 
     def test_get_center(self):
+        """
+        Checks to ensure the center of the image has the same coordinates
+        as the center returned by `get_center`.
+        """
         center = self.fudge.get_center()
         assert(center.x == self.fudge.width/2)
         assert(center.y == self.fudge.height/2)
 
-    def test_select_point(self):
+    def test_select_point_in_image(self):
+        """
+        Checks a single random point to ensure it is within the image
+        bounds.
+        """
         point = self.fudge.select_point()
         self.assertPointInImage(point, self.fudge)
 
-    def test_random_points(self):
-        # test 10 random points
+    def test_ten_random_points(self):
+        """
+        Tests ten random points to ensure they are all within image
+        bounds.
+        """
         points = self.fudge.random_points(10)
         for point in points:
             self.assertPointInImage(point, self.fudge)
+
 
 
 if __name__ == "__main__":
