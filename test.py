8f2c6d935718dbc805ba26d4fb8d21e170c02b74
konstantinfarrell <konstantinvonmolotov@gmail.com>
2016-06-30 22:58:46 -0700

    Add tests for Fudged

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ from imagefudge.image_fudge import Fudged
 TEST_IMAGE = 'examples/GodRoss.jpg'
 
 class TestFudged(unittest.TestCase):
+    """
+    Tests for the Fudged class and class methods.
+    """
+    @staticmethod
+    def assertPointInImage(point, image):
+        assert((point.x < image.width) and (point.y < image.height))
+
     def setUp(self):
         self.fudge = Fudged(TEST_IMAGE)
 
@@ -14,6 +21,17 @@ class TestFudged(unittest.TestCase):
         center = self.fudge.get_center()
         assert(center.x == self.fudge.width/2)
         assert(center.y == self.fudge.height/2)
+
+    def test_select_point(self):
+        point = self.fudge.select_point()
+        self.assertPointInImage(point, self.fudge)
+
+    def test_random_points(self):
+        # test 10 random points
+        points = self.fudge.random_points(10)
+        for point in points:
+            self.assertPointInImage(point, self.fudge)
+
 
 if __name__ == "__main__":
     unittest.main()
