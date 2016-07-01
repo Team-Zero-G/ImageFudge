a409db5f17840b252276d652ca1659294275e951
konstantinfarrell <konstantinvonmolotov@gmail.com>
2016-06-30 22:35:59 -0700

    Add test for center

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 import unittest
+from imagefudge.image_fudge import Fudged
 
-class TestBasic(unittest.TestCase):
-    def test_foo(self):
-        assert(True)
+TEST_IMAGE = 'examples/GodRoss.jpg'
+
+class TestFudged(unittest.TestCase):
+    def setUp(self):
+        self.fudge = Fudged(TEST_IMAGE)
+
+    def tearDown(self):
+        self.fudge.image.close()
+
+    def test_get_center(self):
+        center = self.fudge.get_center()
+        assert(center.x == self.fudge.width/2)
+        assert(center.y == self.fudge.height/2)
 
 if __name__ == "__main__":
     unittest.main()
